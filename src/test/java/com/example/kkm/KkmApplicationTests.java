0e75920bd3f3dbd3b3dac088b4d6987d3f43a63b
seonghwan <norte0190@gmail.com>
2024-02-28 20:26:01 +0900

    init project
    프로젝트 초기화

---
+++ src/test/java/com/example/kkm/KkmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kkm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
