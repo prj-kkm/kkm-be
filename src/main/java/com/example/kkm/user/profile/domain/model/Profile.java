2dd7c05028873a2b35914c6e7901883a48482af6
seonghwan <norte0190@gmail.com>
2024-03-12 02:55:11 +0900

    feat: 프로필 기능 구현

---
+++ src/main/java/com/example/kkm/user/profile/domain/model/Profile.java
@@ -0,0 +1,50 @@
+package com.example.kkm.user.profile.domain.model;
+
+import com.example.kkm.user.domain.User;
+import com.example.kkm.user.profile.domain.ProfileInput;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.AuditOverride;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@AuditOverride(forClass = BaseEntity.class)
+public class Profile extends BaseEntity {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String nickname;
+    private String dogSize;
+    private String zipCode;
+    private String address;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    public static Profile form(ProfileInput form){
+        return Profile.builder()
+            .nickname(form.getNickname())
+            .dogSize(form.getDogSize())
+            .zipCode(form.getZipCode())
+            .address(form.getAddress())
+            .build();
+    }
+}
---
+++ src/main/java/com/example/kkm/user/profile/service/ProfileService.java
@@ -0,0 +1,37 @@
+package com.example.kkm.user.profile.service;
+
+import com.example.kkm.user.exception.CustomException;
+import com.example.kkm.user.exception.ErrorCode;
+import com.example.kkm.user.profile.domain.ProfileInput;
+import com.example.kkm.user.profile.domain.model.Profile;
+import com.example.kkm.user.profile.domain.repository.ProfileRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+
+    public Profile getUserProfile(Long userId) {
+        return profileRepository.findByUser_Id(userId).orElseThrow(
+            () -> new CustomException(ErrorCode.NOT_FOUND_USER));
+    }
+
+
+    public Profile updateUserProfile(Long userId, ProfileInput input ){
+
+        Profile profile = profileRepository.findByUser_Id(userId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+        profile.setNickname(input.getNickname());
+        profile.setDogSize(input.getDogSize());
+        profile.setZipCode(input.getZipCode());
+        profile.setAddress(input.getAddress());
+
+        profileRepository.save(profile);
+        return profile;
+    }
+
+}
