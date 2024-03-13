2d54fa6832f85c1b95939e90b36e6e986977a8ed
seonghwan <norte0190@gmail.com>
2024-03-13 19:35:33 +0900

    feat: User Entity email Column에 고유 제약 조건 추가, 패키지 구조 수정

--- src/main/java/com/example/kkm/user/domain/entity/User.java
+++ src/main/java/com/example/kkm/user/domain/entity/User.java
@@ -1,17 +1,15 @@
-package com.example.kkm.user.auth.entity;
+package com.example.kkm.user.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.util.Collection;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Data
 @Builder
@@ -23,8 +21,9 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int user_id;
-
+    @Column(name = "user_id")
+    private Long id;
+    @Column(unique = true)
     private String email;
 
 }
